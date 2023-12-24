d49a8077fbec652fc75257bffab68ec5c094c04c
Urmasa <n.goltsev@g.nsu.ru>
2023-12-24 22:57:03 +0700

    Update

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 <p>I would like to up my skills as a programmer and add java as own of my maines langueges in my portfolio</p>
 <img src ="./img/roadmap.png">
 <h3>1) Core</h3>
-<h4></h4>
+<h4>Leetcode with java</h4>
+<p>The main idea - solution more and more taskes from leetcode to undestend conceptes</p>
 <h3>2) Advanced</h3>
 <h4></h4>
-<h3>3) Core</h3>
-<h4></h4>
-<h3>4) Core</h3>
+<h3>3) Testing</h3>
+<h4>Modern testing conceptes</h4>
+<h3>4) Add Kotlin</h3>
 <h4></h4>
--- core_models/Classes_concepts/src/Main.java
+++ core_models/Classes_concepts/src/Main.java
@@ -1,9 +1,21 @@
-
-
+import java.util.Scanner;
+import java.util.Arrays;
+// Junit - создание тестов
+// Mockito -
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+
+    }
+}
+
+public class Sulution_sum{
+    public int[] twoSum(int[] nums, int target) {
+        // Sort and creation of another array
+        int arr_of_index = ;
+
+    }
+
+    public int[] make_index_arr(int len){
+        int arr[];
     }
 }
